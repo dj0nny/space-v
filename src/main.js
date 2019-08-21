a84a5d54d4546b9c76369e299f2324fc914eddca
dj0nny <frank96990@gmail.com>
2019-08-21 17:39:16 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue';
+import { plugin } from 'vue-function-api';
+import hooks from '@u3u/vue-hooks';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
+
+Vue.use(hooks);
+Vue.use(plugin);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
