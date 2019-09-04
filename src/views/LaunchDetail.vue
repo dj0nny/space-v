4510aa06f896c046dbfa0297eb7597f2314753d9
dj0nny <frank96990@gmail.com>
2019-09-04 16:41:16 +0200

    edit LaunchDetail.vue

--- src/views/LaunchDetail.vue
+++ src/views/LaunchDetail.vue
@@ -5,8 +5,40 @@
         <b-col v-if="isLoading" md=12 class="text-center">
           <b-spinner label="Spinning"></b-spinner>
         </b-col>
-        <b-col v-if="!isLoading" md=12 class="text-center">
-          {{ currentLaunch }}
+      </b-row>
+      <b-row v-if="!isLoading">
+        <b-col md=6 class="text-center">
+          <img class="mission-patch" :src="currentLaunch.links.mission_patch_small" alt="">
+        </b-col>
+        <b-col md=6 class="text-left details">
+          <h3>Mission Details</h3>
+          <br>
+          <div class="detail">
+            <h6><strong>Mission name:</strong> {{ currentLaunch.mission_name }}</h6>
+          </div>
+          <div class="detail">
+            <h6><strong>Launch year:</strong> {{ currentLaunch.launch_year }}</h6>
+          </div>
+          <div class="detail">
+            <h6><strong>Rocket name:</strong> {{ currentLaunch.rocket.rocket_name }}</h6>
+          </div>
+          <div class="detail">
+            <h6><strong>Rocket type:</strong> {{ currentLaunch.rocket.rocket_type }}</h6>
+          </div>
+        </b-col>
+        <b-col md=12>
+          <h5 class="more-links">Useful links</h5>
+          <ul class="links">
+            <li class="link" >
+              <a class="external" target="_blank" :href="currentLaunch.links.article_link">{{ currentLaunch.links.article_link }}</a>
+            </li>
+            <li class="link" >
+              <a class="external" target="_blank" :href="currentLaunch.links.wikipedia">{{ currentLaunch.links.wikipedia }}</a>
+            </li>
+            <li class="link" >
+              <a class="external" target="_blank" :href="currentLaunch.links.video_link">{{ currentLaunch.links.video_link }}</a>
+            </li>
+          </ul>
         </b-col>
       </b-row>
     </b-container>
@@ -37,3 +69,22 @@ export default {
   },
 };
 </script>
+
+
+<style scoped>
+h5.more-links {
+  margin-top: 60px;
+}
+
+a.external {
+  color: #000 !important;
+}
+
+.detail {
+  margin-bottom: 30px;
+}
+
+.details .detail:last-child {
+  margin-bottom: 0px;
+}
+</style>
