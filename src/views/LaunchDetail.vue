9c264fbc530a7deac4f03b88fbcdfe5fe13d7cc9
dj0nny <frank96990@gmail.com>
2019-09-12 18:05:48 +0200

    edit LaunchDetail.vue

--- src/views/LaunchDetail.vue
+++ src/views/LaunchDetail.vue
@@ -10,9 +10,7 @@
         <b-col md=6 class="text-center">
           <img class="mission-patch" :src="currentLaunch.links.mission_patch_small" alt="">
         </b-col>
-        <b-col md=6 class="text-left details">
-          <h3>Mission Details</h3>
-          <br>
+        <b-col md=3 class="text-left details">
           <div class="detail">
             <h6><strong>Mission name:</strong> {{ currentLaunch.mission_name }}</h6>
           </div>
@@ -25,9 +23,22 @@
           <div class="detail">
             <h6><strong>Rocket type:</strong> {{ currentLaunch.rocket.rocket_type }}</h6>
           </div>
+          <div class="detail">
+            <h6><strong>Launch site:</strong> {{ currentLaunch.launch_site.site_name_long }}</h6>
+          </div>
+        </b-col>
+        <b-col md=3>
+          <div class="mission-status text-center">
+            <b-alert show variant="success" v-if="currentLaunch.launch_success">Success</b-alert>
+            <b-alert show variant="danger" v-else>Aborted</b-alert>
+          </div>
         </b-col>
         <b-col md=12>
-          <h5 class="more-links">Useful links</h5>
+          <h5 class="subtitle">Details</h5>
+          {{ currentLaunch.details }}
+        </b-col>
+        <b-col md=12>
+          <h5 class="subtitle more-links">Useful links</h5>
           <ul class="links">
             <li class="link" >
               <a class="external" target="_blank" :href="currentLaunch.links.article_link">{{ currentLaunch.links.article_link }}</a>
@@ -59,7 +70,7 @@ export default {
     const { route } = useRouter();
 
     onCreated(async () => {
-      FETCH_LAUNCH_DETAIL(route.value.params.id);
+      await FETCH_LAUNCH_DETAIL(route.value.params.id);
     });
 
     return {
@@ -72,8 +83,9 @@ export default {
 
 
 <style scoped>
-h5.more-links {
-  margin-top: 60px;
+.subtitle {
+  margin-top: 30px;
+  font-weight: bold;
 }
 
 a.external {
